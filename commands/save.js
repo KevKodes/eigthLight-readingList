2bb1d780afd841a40ed39a4570173edccedd6c5b
Kevin <pitzer.kevinj@gmail.com>
2021-06-14 11:03:57 -0400

    clean up console logs

--- commands/save.js
+++ commands/save.js
@@ -8,13 +8,14 @@ const saveBookCB = (err) => {
 };
 
 const saveCmd = async (bookId) => {
+  console.log(`Saving book ${bookId} to reading list`);
+
   try {
     const bookURL = `https://www.googleapis.com/books/v1/volumes/${bookId}`;
     const book = await axios.get(bookURL);
-    // console.log("found the book: ", book);
     const bookData = `Title: ${book.data.volumeInfo.title}\nAuthor(s): ${book.data.volumeInfo.authors}\nPublisher: ${book.data.volumeInfo.publisher}\nID: ${book.data.id}`;
-    console.log(`Saving to reading list: ${bookData}`);
     await fs.appendFile("reading_list.txt", bookData, saveBookCB);
+    console.log(`Success. Saved to reading list: ${bookData}`);
   } catch (error) {
     console.error(`An error occurred. ${error}`);
   }
--- commands/search.js
+++ commands/search.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const searchCmd = async (keyword) => {
+  console.log("Searching library for keyword: ", keyword);
+
   try {
-    // check the keyword and throw an error if it is no bueno
     const searchURL = `https://www.googleapis.com/books/v1/volumes?q=${keyword}&maxResults=5`;
     const returnList = await axios.get(searchURL);
     returnList.data.items.forEach((book) => {
