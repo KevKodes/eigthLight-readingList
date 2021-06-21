4cc1cbfb3eb69d70a258818ce572b5dc3c37cd6d
Kevin <pitzer.kevinj@gmail.com>
2021-06-21 15:52:30 -0400

    add basic readline interface

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ import saveCmd from "./commands/save.js";
 import helpCmd from "./commands/help.js";
 import viewCmd from "./commands/view.js";
 
+import readline from "readline";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "OHAI> ",
+});
+
+rl.prompt();
+
+rl.on("line", (line) => {
+  switch (line.trim()) {
+    case "hello":
+      console.log("world!");
+      break;
+    default:
+      console.log(`Say what? I might have heard '${line.trim()}'`);
+      break;
+  }
+  rl.prompt();
+}).on("close", () => {
+  console.log("Have a great day!");
+  process.exit(0);
+});
+
 const libraryDirectory = () => {
   const myArgs = process.argv.slice(2);
 
