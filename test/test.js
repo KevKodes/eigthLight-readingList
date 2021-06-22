4ec474107215cdfbd57f31a4162503f6a15a8b54
Kevin <pitzer.kevinj@gmail.com>
2021-06-22 17:21:54 -0400

    notes on tests in progress

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ import { expect } from "chai";
 import sinon from "sinon";
 import searchCmd from "../commands/search.js";
 import saveCmd from "../commands/save.js";
-import fs from "fs";
-import readline from "readline";
+import viewCmd from "../commands/view.js";
 import helpCmd from "../commands/help.js";
+import fs from "fs";
 
 // Search command should get 5 items relating to the query
 describe("Search Command", function () {
@@ -19,18 +19,38 @@ describe("Search Command", function () {
 // Save command should append the book to the text file
 describe("Save Command", function () {
   before(() => saveCmd("HznMDwAAQBAJ"));
-  it("Should save the book to the reading-list", function () {
-    const save = fs.readFileSync("reading_list.txt").toString();
+  it("Should save the book to the reading-list", async function () {
+    const save = await fs.readFileSync("reading_list.txt").toString();
     expect(save).to.contain("HznMDwAAQBAJ");
   });
 });
 
-// Each command should launch it's own function
+/* 
+The following tests are still under development. They should incorporate the 
+use of mock functions to fake user input in readline and spy on the command
+functions to ensure the correct ones are called.
+
+Each command should launch it's own function
+*/
 describe("General functionality", function () {
+  it("Should call the help function when the user enters 'search'", function () {
+    const spied = sinon.spy(searchCmd);
+    spied();
+    expect(spied.called).to.be.true;
+  });
+  it("Should call the help function when the user enters 'save'", function () {
+    const spied = sinon.spy(saveCmd);
+    spied();
+    expect(spied.called).to.be.true;
+  });
+  it("Should call the help function when the user enters 'view'", function () {
+    const spied = sinon.spy(viewCmd);
+    spied();
+    expect(spied.called).to.be.true;
+  });
   it("Should call the help function when the user enters help", function () {
     const spied = sinon.spy(helpCmd);
-    spied(); // change this to the user calling from readline
-    // await readlineCb("help");
+    spied();
     expect(spied.called).to.be.true;
   });
 });
